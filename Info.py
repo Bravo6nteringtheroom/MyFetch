c1a3c321bb3b9ea59e8d1dec6009508e64668a86
Bravo6nteringtheroom <mohamadOwidate2@gmail.com>
2025-12-12 00:30:11 +0200

    Welcome to myFetch

---
+++ Info.py
@@ -0,0 +1,31 @@
+import platform
+import psutil
+import os
+import socket
+from uptime import uptime
+
+class OS:
+    #OS info 
+    OS_name = platform.platform()
+    OS_version = platform.system()
+    
+    #Host name
+    Hostname = socket.gethostname()
+    
+    #CPU info
+    cpu_cores = os.process_cpu_count()
+    cpu_type = platform.processor()
+    cpu_usage = psutil.cpu_percent(interval=1)
+    #RAM usage / total
+    ram_used = round(psutil.virtual_memory().percent)
+    ram_total = round(psutil.virtual_memory().total / (1024 ** 3))
+    
+    #IPAddr
+    IP_Addr = socket.gethostbyname(Hostname)
+    
+    #Disk used / total
+    storage_used = round((psutil.disk_usage('/').used / (1024 ** 3)))
+    storage_total = round((psutil.disk_usage('/').total / (1024 ** 3)))
+    
+    #UpTime function
+    uptime = round(uptime() / 3600)
---
+++ main.py
@@ -0,0 +1,38 @@
+import Logos
+from uptime import uptime
+from itertools import zip_longest
+from Info import OS
+import icons
+
+
+p = OS()
+
+info_lines = [
+    #Hostname:
+    f"\033[1;37m{icons.host_icon} Host Name: {p.Hostname}\033[0m",
+    #IP Address:
+    f"\033[1;33m{icons.IP_icon} IP Address: {p.IP_Addr}\033[0m",
+    #OS_name:
+    f"\033[1;34m{icons.window_icon} OS: {p.OS_name}\033[0m",
+    #OS_version:
+    f"\033[1;36m{icons.version_icon} Version: {p.OS_version}\033[0m",
+    #cpu_type
+    f"\033[1;35m{icons.cpu_icon} CPU Type: {p.cpu_type}\033[0m",
+    #cpu_cores
+    f"\033[1;32m{icons.core_icon} CPU Core: {p.cpu_cores}\033[0m",
+    #cpu_usage
+    f"\033[1;31m{icons.usage_icon} CPU Usage: {p.cpu_usage}%\033[0m",
+    #ram_used / ram_total
+    f"\033[1;33m{icons.ram_icon} RAM: {p.ram_used}%\033[0m",
+    #sotrageused / storagetotal
+    f"\033[1;36m{icons.usage_icon} DISK: {p.storage_used} / {p.storage_total} GB\033[0m",
+    #Get the uptime
+    f"\033[1;32m{icons.clock_icon} UpTime: {p.uptime}Hr\033[0m"
+]
+
+def Display(logo_icon , info_sys):
+    print()
+    for logo, info in zip_longest(logo_icon, info_sys, fillvalue=""):
+        print(f"{logo}\t \t{info}")
+        
+Display(Logos.logo_lines2,info_lines)
